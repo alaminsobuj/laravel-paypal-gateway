55e814c9b8997c682493acdfee61a65e0ded5955
alaminsobuj8 <alaminsobuj8@gmail.com>
2022-09-06 11:57:14 +0600

    laravel with paypal payment gateway

---
+++ config/paypal.php
@@ -0,0 +1,12 @@
+<?php 
+return [ 
+    'client_id' => 'ARf-lCh7rjrgaAPGnzDYFImoRtG6HsmvmpibKopxtgmnKJ-pXzPBIJVXMHO0PuYjQmYjjfiL8H-VXX_G',
+	'secret' => 'EKNSEN-Vzef7peT_mrsJsMTUVkoiWiKbVYQ28VifE23KdrPzDcVrGjwlrJe07nsnstaJDSmc_bUenDtP',
+    'settings' => array(
+        'mode' => 'sandbox',
+        'http.ConnectionTimeOut' => 1000,
+        'log.LogEnabled' => true,
+        'log.FileName' => storage_path() . '/logs/paypal.log',
+        'log.LogLevel' => 'FINE'
+    ),
+];
